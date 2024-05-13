58a48e097886c31fcab1c1a4de62456fc029c660
Rajusharma2003 <raju2003sharmars3@gmail.com>
2024-05-13 19:07:41 +0530

    implement the modern useState and explore it

--- pokedex_project/pokemon_api_handle/src/Component/PokemonList/PokemonL.jsx
+++ pokedex_project/pokemon_api_handle/src/Component/PokemonList/PokemonL.jsx
@@ -1,91 +1,98 @@
 
-/** issme hum useEffect hook ka use kar rhe hai useEffect page kai fist load pai kisi
- * bhi (Api) call and (download) karta hai => useEffect kai ander ek callback funtion and 
- * and dependence array[] hota hai agar hum yai dependence array[] kai isske ander naa de to
- * page kai har reload pai woo (api) call and kisi bhi file ko download karta hai or iss 
- * dependence array[] mai hum kisi variable ko bhi dai skte hai ki kisi variable pai kisi 
- * type ka change hota hi page reloade hoga or. 
- */
-
-import { useEffect, useState } from 'react'
-import './PokemonL.css'
-import axios from 'axios'
-import ShowPokeData from '../ShowPokeData/ShowPokeData'
+import { useEffect, useState } from 'react';
+import './PokemonL.css';
+import axios from 'axios';
+import ShowPokeData from '../ShowPokeData/ShowPokeData';
 
 function PokemonList() {
 
-    const [pokemonList , setPokemonList ] = useState([])
-    const [isLoading , setIsLoading ] = useState([true])
+       // const [pokemonList , setPokemonList ] = useState([])
+    // const [isLoading , setIsLoading ] = useState([true])
 
+    // const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
 
-    const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+    // const [prevUrl , setPrevUrl] = useState('');//2
+    // const [nextUrl , setNextUrl] = useState(''); //2
 
-    const [prevUrl , setPrevUrl] = useState('');//2
-    const [nextUrl , setNextUrl] = useState(''); //2
-
+/** if you can not use lot of the useState then you can use the morden ("useSatate").  */
+    const [userStatesList, setUserStatesList] = useState({
+        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
+        pokemonList: [],
+        isLoading: true,
+        prevUrl: '',
+        nextUrl: ''
+    });
 
     async function downloadPokemons() {
-        setIsLoading(true)
-        const res = await axios.get(pokedexUrl);
+        // setIsLoading(true)
+        setUserStatesList({ ...userStatesList, isLoading: true });
+
+        const res = await axios.get(userStatesList.pokedexUrl);
         const pokemonResult = res.data.results;
 
-        setNextUrl(res.data.next); //2
-        setPrevUrl(res.data.previous); //2
-        
-        const pokemonResultPromise = pokemonResult.map( async ( poke) => await axios.get(poke.url));   // yaha hum pokemonResult sai 20 pokemon data ko ek array mai dal rahe hai or only uska "url" nikal rahe hai 
-        const pokemonFinalData =  await axios.all(pokemonResultPromise)
-        
-        console.log(pokemonFinalData);  // iss data kai ander {"next"} and {"prev"} milta hai
-        
+          // setNextUrl(res.data.next); //2
+        // setPrevUrl(res.data.previous); //2
+        setUserStatesList( (states) => ({
+            ...states,
+            prevUrl: res.data.previous,
+            nextUrl: res.data.next
+        }));
+
+        const pokemonResultPromise = pokemonResult.map(async (poke) => await axios.get(poke.url));
+        const pokemonFinalData = await axios.all(pokemonResultPromise);
 
         const pokemonDataFilter = pokemonFinalData.map((onePokeElement) => {
-            const pokemonData = onePokeElement.data
-            //  yaha per hum onePokeElementData kai ander sai data ko parse kar rhe hai.
-            return { 
-                id : pokemonData.id,
-                name : pokemonData.name ,
-                types : pokemonData.types,
-                image : (pokemonData.sprites.other) ? pokemonData.sprites.other.dream_world.front_default : pokemonData.sprites.front_shiny
-             }
-        })
-        console.log(pokemonDataFilter);
-        setPokemonList(pokemonDataFilter);
-        setIsLoading(false);
-
+            const pokemonData = onePokeElement.data;
+            return {
+                id: pokemonData.id,
+                name: pokemonData.name,
+                types: pokemonData.types,
+                image: (pokemonData.sprites.other)
+                    ? pokemonData.sprites.other.dream_world.front_default
+                    : pokemonData.sprites.front_shiny
+            };
+        });
+        // console.log(pokemonDataFilter);
+       // setPokemonList(pokemonDataFilter);
+        // setIsLoading(false);.
+        setUserStatesList( (states) =>({
+            ...states,
+            pokemonList: pokemonDataFilter,
+            isLoading: false
+        }));
     }
 
+    useEffect(() => {
+        downloadPokemons();
+    }, [userStatesList.pokedexUrl]);
 
-    // useEffect hook in React.js.
-    useEffect( () => {
-        downloadPokemons()
-        
-    } , [pokedexUrl])
+return (
+     <div className='pokemon_list_wrapper'>
+            <div className="pokemon_wrapper">
+                {(userStatesList.isLoading)
+                    ? 'Loading...'
+                    : userStatesList.pokemonList.map((p) => (
+                        <ShowPokeData name={p.name} image={p.image} key={p.id} id={p.id} />
+                    ))}
+            </div>
 
+            <div className="controls">
 
+                 {/* <button disabled ={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)} >prev</button>
+                 <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)}>next</button> */}
 
-    return (
+                <button disabled={userStatesList.prevUrl == null} onClick={() => {
+                    const urlToSet = userStatesList.prevUrl
+                    setUserStatesList({ ...userStatesList, pokedexUrl: urlToSet })
+                }}>prev</button>
 
-        <div className='pokemon_list_wrapper'>
-
-            
-        {/* // conditional rendering */}
-
-        <div className="pokemon_wrapper">
-        {
-        (isLoading) ? 'Loading...'
-        : pokemonList.map( ( p) => <ShowPokeData  name={p.name} image={p.image} key={p.id} id={p.id}/>) 
-        }
-        </div>
-        
-
-        <div className="controls">
-            <button disabled ={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)} >prev</button>
-            <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)}>next</button>
-        </div>
-
-        </div>
-    )
+                 <button disabled={userStatesList.nextUrl == null} onClick={() => {
+                    const urlToSet = userStatesList.nextUrl
+                    setUserStatesList({ ...userStatesList, pokedexUrl: urlToSet })
+                }}>Next</button>
+    </div>
+     </div>
+    );
 }
 
-
-export default PokemonList
+export default PokemonList;
