3fe4cafeaef3dd0e394e0851e8658fd6c946bcc0
Rajusharma2003 <raju2003sharmars3@gmail.com>
2024-05-14 17:38:51 +0530

    interduce the custom Hook in React

--- pokedex_project/pokemon_api_handle/src/Component/PokemonList/PokemonL.jsx
+++ pokedex_project/pokemon_api_handle/src/Component/PokemonList/PokemonL.jsx
@@ -3,68 +3,11 @@ import { useEffect, useState } from 'react';
 import './PokemonL.css';
 import axios from 'axios';
 import ShowPokeData from '../ShowPokeData/ShowPokeData';
+import usePokemonList from '../../hooks/usePokemonList';
 
 function PokemonList() {
 
-       // const [pokemonList , setPokemonList ] = useState([])
-    // const [isLoading , setIsLoading ] = useState([true])
-
-    // const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
-
-    // const [prevUrl , setPrevUrl] = useState('');//2
-    // const [nextUrl , setNextUrl] = useState(''); //2
-
-/** if you can not use lot of the useState then you can use the morden ("useSatate").  */
-    const [userStatesList, setUserStatesList] = useState({
-        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
-        pokemonList: [],
-        isLoading: true,
-        prevUrl: '',
-        nextUrl: ''
-    });
-
-    async function downloadPokemons() {
-        // setIsLoading(true)
-        setUserStatesList({ ...userStatesList, isLoading: true });
-
-        const res = await axios.get(userStatesList.pokedexUrl);
-        const pokemonResult = res.data.results;
-
-          // setNextUrl(res.data.next); //2
-        // setPrevUrl(res.data.previous); //2
-        setUserStatesList( (states) => ({
-            ...states,
-            prevUrl: res.data.previous,
-            nextUrl: res.data.next
-        }));
-
-        const pokemonResultPromise = pokemonResult.map(async (poke) => await axios.get(poke.url));
-        const pokemonFinalData = await axios.all(pokemonResultPromise);
-
-        const pokemonDataFilter = pokemonFinalData.map((onePokeElement) => {
-            const pokemonData = onePokeElement.data;
-            return {
-                id: pokemonData.id,
-                name: pokemonData.name,
-                types: pokemonData.types,
-                image: (pokemonData.sprites.other)
-                    ? pokemonData.sprites.other.dream_world.front_default
-                    : pokemonData.sprites.front_shiny
-            };
-        });
-        // console.log(pokemonDataFilter);
-       // setPokemonList(pokemonDataFilter);
-        // setIsLoading(false);.
-        setUserStatesList( (states) =>({
-            ...states,
-            pokemonList: pokemonDataFilter,
-            isLoading: false
-        }));
-    }
-
-    useEffect(() => {
-        downloadPokemons();
-    }, [userStatesList.pokedexUrl]);
+    const { userStatesList, setUserStatesList} = usePokemonList();
 
 return (
      <div className='pokemon_list_wrapper'>
---
+++ pokedex_project/pokemon_api_handle/src/hooks/usePokemonList.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useState , useEffect} from "react";
+
+function usePokemonList() {
+
+       // const [pokemonList , setPokemonList ] = useState([])
+    // const [isLoading , setIsLoading ] = useState([true])
+
+    // const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+
+    // const [prevUrl , setPrevUrl] = useState('');//2
+    // const [nextUrl , setNextUrl] = useState(''); //2
+
+/** if you can not use lot of the useState then you can use the morden ("useSatate").  */
+const [userStatesList, setUserStatesList] = useState({
+    pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
+    pokemonList: [],
+    isLoading: true,
+    prevUrl: '',
+    nextUrl: ''
+});
+
+async function downloadPokemons() {
+    // setIsLoading(true)
+    setUserStatesList({ ...userStatesList, isLoading: true });
+
+    const res = await axios.get(userStatesList.pokedexUrl);
+    const pokemonResult = res.data.results;
+
+      // setNextUrl(res.data.next); //2
+    // setPrevUrl(res.data.previous); //2
+    setUserStatesList( (states) => ({
+        ...states,
+        prevUrl: res.data.previous,
+        nextUrl: res.data.next
+    }));
+
+    const pokemonResultPromise = pokemonResult.map(async (poke) => await axios.get(poke.url));
+    const pokemonFinalData = await axios.all(pokemonResultPromise);
+
+    const pokemonDataFilter = pokemonFinalData.map((onePokeElement) => {
+        const pokemonData = onePokeElement.data;
+        return {
+            id: pokemonData.id,
+            name: pokemonData.name,
+            types: pokemonData.types,
+            image: (pokemonData.sprites.other)
+                ? pokemonData.sprites.other.dream_world.front_default
+                : pokemonData.sprites.front_shiny
+        };
+    });
+    // console.log(pokemonDataFilter);
+   // setPokemonList(pokemonDataFilter);
+    // setIsLoading(false);.
+    setUserStatesList( (states) =>({
+        ...states,
+        pokemonList: pokemonDataFilter,
+        isLoading: false
+    }));
+}
+
+useEffect(() => {
+    downloadPokemons();
+}, [userStatesList.pokedexUrl]);
+
+
+return {userStatesList , setUserStatesList}
+
+}
+
+
+export default usePokemonList
